cddc788b8d21468e31743546c71b517bdb4da7db
Thanh Huy <vothanhhuy7102004@gmail.com>
2025-12-17 00:59:06 +0700

    big update 1

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api =axios.create({
-    baseURL:"https://deloywebreactnodejs.onrender.com/",
+    baseURL:"https://deloywebreactnodejs.onrender.com",
 })
 
 export default api;
