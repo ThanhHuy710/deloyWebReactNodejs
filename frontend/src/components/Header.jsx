9b74cbcd20f4bcff2fc4ec745a1f0c08274fa697
Thanh Huy <vothanhhuy7102004@gmail.com>
2025-12-17 01:39:33 +0700

    them ten vao header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ export default function Header() {
     <>
       <div className="space-y-2 text-center ">
           <h1 className="text-4xl font-bold text-transparent bg-primary bg-clip-text">todoX</h1>
+          <p>Võ Thanh Huy</p>
           <p>quản lý tasks đơn giản hơn</p>
       </div>
     </>
